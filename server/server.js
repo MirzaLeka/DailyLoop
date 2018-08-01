e612fadf05f09c1863ecdb775588017870ca4101
Mirza Leka <mirza_376@hotmail.com>
2018-08-02 01:15:27 +0200

    Added get single todo route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -34,6 +35,27 @@ app.get('/todos', (req, res) => {
   }, (e) => { // and once we pull off GET request on frontend, our data will be stored in variable todos, just like we named it
     res.status(400).send(e);
   });
+});
+
+app.get("/todos/:id", (req, res) => {
+var id = req.params.id;
+
+if (!ObjectID.isValid(id)) {
+ return res.status(400).send('Id is not valid'); 
+}
+
+Todo.findById(id).then(todo => {
+  if (!todo) {
+     return res.status(404).send("Id not found");
+   }
+  res.send({todo}); // if all went well we're getting desired todo
+
+  }).catch(e => {
+  if (e) {
+    return res.status(400).send("Something went wrong");
+    }
+ });
+
 });
 
 app.listen(3000, () => {
