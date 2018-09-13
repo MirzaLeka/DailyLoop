c2c1d9a547e87263f05a2d064c28bf989fef66c3
Mirza Leka <mirza_376@hotmail.com>
2018-09-13 15:51:44 +0200

    Setup basic sign up validation with jquery validate

--- Resources/js/login.js
+++ Resources/js/login.js
@@ -11,13 +11,16 @@ function changeForm(num) {
         
         <h2 class="formHeader">Sign Up</h2>
     
-        <input class="loginInput" type="text" placeholder="Username" autofocus required/>
-        
-        <input class="loginInput" type="email" placeholder="Email" required/>
+        <input class="loginInput" type="text" placeholder="Username" id="signupUsername" autofocus required/>
+        <p class="error" id="errorUsername"></p>
 
-        <input class="loginInput" type="password" placeholder="Password" required/>
+        <input class="loginInput" type="email" placeholder="Email" id="signupEmail" required/>
+        <p class="error" id="errorEmail"></p>
 
-        <button class="btn btn-success formBtn" id="confirmBtn">CONFIRM</button>
+        <input class="loginInput" type="password" placeholder="Password" id="signupPassword" required/>
+        <p class="error" id="errorPassword"></p>
+
+        <button class="btn btn-success formBtn" onclick="signup()">CONFIRM</button>
 
         <p class="changeForm" onclick="changeForm(1)">Already a member?</p>
         
@@ -44,3 +47,53 @@ function changeForm(num) {
     }
 
 }
+
+
+$(function() {
+
+$("#signup-form").validate({
+    rules: {
+        signupUsername: {
+            required: true
+        },
+        signupEmail: {
+            required: true,
+            email: true
+        },
+        signupPassword: {
+            required: true
+        }
+    }
+
+
+});
+
+});
+
+
+// function signup() {
+
+//     let username = document.getElementById("signupUsername").value;
+//     let usernameError = document.getElementById("errorUsername");
+
+//     if (username == "") {
+//         usernameError.style.display = "block";
+//         usernameError.innerHTML = "Something"
+//     }
+
+// // if member already exists print error
+
+// }
+
+
+// function clearErrors() {
+
+// }
+
+
+// function login() {
+
+// // if member doesn't exist print error
+
+// }
+
