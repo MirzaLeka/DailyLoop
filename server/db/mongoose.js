191c6a1e24c3dcbc1bb4b9787e204e75c37a0763
Mirza Leka <mirza_376@hotmail.com>
2018-09-21 19:44:20 +0200

    Settingup mongoose for mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; 
-mongoose.connect('mongodb://localhost:27017/TodoApp'); 
+mongoose.connect('mongodb://<Spy4er>:<Bu11$Eye44>@ds111113.mlab.com:11113/todoapp'); 
+
+// 'mongodb://localhost:27017/TodoApp'
 
 module.exports = {mongoose};
 // instead of configuring server and mongoose in file, we made a separate file for mongoose that we'll include in server.js file
